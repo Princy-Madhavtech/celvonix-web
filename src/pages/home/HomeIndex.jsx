7e4ca091845203b16ec6ada6d6ed1e9d7478cc61
PrincySuhagiya-ashirwad <princy.suhagiya@ashirwadinfotech.com>
2025-09-09 11:21:45 +0530

    project setup

--- src/pages/home/HomeIndex.jsx
+++ src/pages/home/HomeIndex.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Header from './Header'
+import AboutSection from './AboutSection'
+import Footer from '../../component/global/Footer'
 
 const HomeIndex = () => {
   return (
     <div className='min-h-screen flex items-center justify-center'>
-    
+    <Header/>
+    <AboutSection/>
+    <Footer/>
     </div>
   )
 }
